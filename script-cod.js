180488c0f9b650e14f1db82d5f3718490b44d1c3
gacekid <121511858+gacekid@users.noreply.github.com>
2022-12-28 22:54:32 +0700

    Create script-cod.js

---
+++ script-cod.js
@@ -0,0 +1,9 @@
+var ongkirCOD = [
+	{"nama":"Desa Tanjungkerta", "harga":"10000"},
+	{"nama":"Desa C", "harga":"11000"},
+	{"nama":"Desa D", "harga":"12000"},
+	{"nama":"Desa E", "harga":"13000"},
+	{"nama":"Desa F", "harga":"14000"},
+	{"nama":"Desa G", "harga":"15000"},
+	{"nama":"Desa H", "harga":"16000"},
+	];
